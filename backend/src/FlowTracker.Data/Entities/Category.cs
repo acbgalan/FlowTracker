9d2460be494e38e89b63bf3fe72d483cafc1be38
Carlos Blanco <acbgalan@gmail.com>
2026-02-21 01:33:14 +0100

    feat: Added Category and Transaction entities

---
+++ backend/src/FlowTracker.Data/Entities/Category.cs
@@ -0,0 +1,17 @@
+﻿using FlowTracker.Shared.Enums;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FlowTracker.Data.Entities
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public required string Name { get; set; }
+        public TransactionType Type { get; set; }
+        public string Icon { get; set; } = string.Empty;
+
+        public ICollection<Transaction> Transactions { get; set; } = new List<Transaction>();
+    }
+}
