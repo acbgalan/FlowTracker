11cc34f8c56ae83ebdcabe7340b2c6092e5282e2
Carlos Blanco <acbgalan@gmail.com>
2026-02-22 01:00:50 +0100

    feat: Enum to represent the type of transaction (income/expense)

---
+++ backend/src/FlowTracker.Shared/Enums/TransactionType.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FlowTracker.Shared.Enums
+{
+    public enum TransactionType
+    {
+        Income = 0,
+        Expense = 1
+    }
+}
