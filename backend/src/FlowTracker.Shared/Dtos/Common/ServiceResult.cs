ccbd8dd7fbbff4e49f7e465746b16756e04a2c79
Carlos Blanco <acbgalan@gmail.com>
2026-02-24 00:56:58 +0100

    feat: Classes to implement the Service Response pattern.

---
+++ backend/src/FlowTracker.Shared/Dtos/Common/ServiceResult.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FlowTracker.Shared.Dtos.Common
+{
+    public class ServiceResult
+    {
+        public bool Success { get; set; }
+        public string Message { get; set; } = string.Empty;
+        public int StatusCode { get; set; }
+    }
+}
