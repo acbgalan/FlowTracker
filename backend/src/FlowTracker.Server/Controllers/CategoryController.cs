38d9341531d046f64322dd5c26a42ab3c409bbbf
Carlos Blanco <acbgalan@gmail.com>
2026-02-25 00:51:06 +0100

    feat: Methods to retrieve categories and DTO to return categories.

---
+++ backend/src/FlowTracker.Server/Controllers/CategoryController.cs
@@ -0,0 +1,51 @@
+﻿using AutoMapper;
+using FlowTracker.Data.Entities;
+using FlowTracker.Data.Repositories;
+using FlowTracker.Shared.Dtos.Category;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FlowTracker.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoryController : ControllerBase
+    {
+        private readonly ICategoryRepository _categoryRepository;
+        private readonly IMapper _mapper;
+
+        public CategoryController(ICategoryRepository categoryRepository, IMapper mapper)
+        {
+            _categoryRepository = categoryRepository;
+            _mapper = mapper;
+        }
+
+        [HttpGet("{id:int}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<ActionResult<CategoryResponse>> GetCategory(int id)
+        {
+            var category = await _categoryRepository.GetAsync(id);
+
+            if (category == null)
+            {
+                return NotFound("Category not found");
+            }
+
+            var categoryResponse = _mapper.Map<CategoryResponse>(category);
+
+            return Ok(categoryResponse);
+        }
+
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public async Task<ActionResult<List<CategoryResponse>>> GetAllCategories()
+        {
+            var categories = await _categoryRepository.GetAllAsync();
+            var categoriesResponse = _mapper.Map<List<CategoryResponse>>(categories);
+
+            return Ok(categoriesResponse);
+        }
+
+    }
+}
---
+++ backend/src/FlowTracker.Shared/Dtos/Category/CategoryResponse.cs
@@ -0,0 +1,15 @@
+﻿using FlowTracker.Shared.Enums;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FlowTracker.Shared.Dtos.Category
+{
+    public class CategoryResponse
+    {
+        public int Id { get; set; }
+        public required string Name { get; set; }
+        public string Type { get; set; } = string.Empty;
+        public string Icon { get; set; } = string.Empty;
+    }
+}
