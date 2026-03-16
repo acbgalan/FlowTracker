8ce6f8a154f0e326179996faa70800c4892ba902
Carlos Blanco <acbgalan@gmail.com>
2026-03-16 15:54:01 +0100

    feat: User class with First Name and Last Name fields. Class inherits from IdentityUser and will use on other entities

---
+++ backend/src/FlowTracker.Data/Entities/User.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FlowTracker.Data.Entities
+{
+    public sealed class User : IdentityUser
+    {
+        public required string FirstName { get; set; }
+        public string? LastName { get; set; }
+    }
+}
