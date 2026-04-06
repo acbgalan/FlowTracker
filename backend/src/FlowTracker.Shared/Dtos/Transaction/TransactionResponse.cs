2acc83e5fd6250e5b79e4623a69191e572be3177
Carlos Blanco <acbgalan@gmail.com>
2026-04-06 03:22:45 +0200

    feat: TransactionResponse

---
+++ backend/src/FlowTracker.Shared/Dtos/Transaction/TransactionResponse.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FlowTracker.Shared.Dtos.Transaction
+{
+    public class TransactionResponse
+    {
+        public int Id { get; set; }
+        public required decimal Amount { get; set; }
+        public DateOnly Date { get; set; }
+        public string? Description { get; set; }
+        public required string CategoryName { get; set; }
+        public required string Type { get; set; }
+        public required string UserId { get; set; }
+    }
+}
