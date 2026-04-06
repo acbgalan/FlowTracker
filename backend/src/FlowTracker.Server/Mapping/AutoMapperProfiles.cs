f6c8d7b4ae798b2d18e23697c0f95a98faeef3eb
Carlos Blanco <acbgalan@gmail.com>
2026-04-06 18:00:08 +0200

    feat: AutoMapper configuration. Transaction - TransactionResponse

--- backend/src/FlowTracker.Server/Mapping/AutoMapperProfiles.cs
+++ backend/src/FlowTracker.Server/Mapping/AutoMapperProfiles.cs
@@ -1,6 +1,7 @@
 ﻿using AutoMapper;
 using FlowTracker.Data.Entities;
 using FlowTracker.Shared.Dtos.Category;
+using FlowTracker.Shared.Dtos.Transaction;
 using FlowTracker.Shared.Dtos.User;
 
 namespace FlowTracker.Server.Mapping
@@ -11,6 +12,7 @@ namespace FlowTracker.Server.Mapping
         {
             CategoryMappings();
             UserMappings();
+
         }
 
         private void CategoryMappings()
@@ -25,5 +27,15 @@ namespace FlowTracker.Server.Mapping
             CreateMap<UserRegisterRequest, User>()
                 .ForMember(d => d.UserName, o => o.MapFrom(s => s.Email));
         }
+
+        private void TransactionMappings()
+        {
+            CreateMap<Transaction, TransactionResponse>()
+                .ForMember(d => d.CategoryName, o => o.MapFrom(s => s.Category.Name))
+                .ForMember(d => d.Type, o => o.MapFrom(s => s.Category.Type))
+                .ForMember(d => d.Icon, o => o.MapFrom(s => s.Category.Icon))
+                .ForMember(d =>> d.CategoryDescription, o => o.MapFrom(s => s.Category.Description));
+        }
+
     }
 }
