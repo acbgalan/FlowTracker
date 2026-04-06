07be4a6336666e8dfb89a2a7936d6bec61ee282f
Carlos Blanco <acbgalan@gmail.com>
2026-04-07 01:37:58 +0200

    feat: DTO to create a new transaction.

---
+++ backend/src/FlowTracker.Shared/Dtos/Transaction/CreateTransactionRequest.cs
@@ -0,0 +1,15 @@
+﻿using FlowTracker.Shared.Enums;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FlowTracker.Shared.Dtos.Transaction
+{
+    public class CreateTransactionRequest
+    {
+        public decimal Amount { get; set; }
+        public DateOnly Date { get; set; }
+        public string? Description { get; set; }
+        public int CategoryId { get; set; }
+    }
+}
