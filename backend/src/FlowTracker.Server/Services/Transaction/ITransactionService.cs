209083a60022a356916b30eb50813b153548e2d5
Carlos Blanco <acbgalan@gmail.com>
2026-04-06 18:02:14 +0200

    feat: Retrieve a transaction by ID. TransactionService

---
+++ backend/src/FlowTracker.Server/Services/Transaction/ITransactionService.cs
@@ -0,0 +1,14 @@
+﻿using FlowTracker.Data.Entities;
+using FlowTracker.Shared.Dtos.Common;
+using FlowTracker.Shared.Dtos.Transaction;
+
+namespace FlowTracker.Server.Services.Transaction
+{
+    public interface ITransactionService
+    {
+        Task<ServiceResult<TransactionResponse>> GetTransaction(int id);
+        Task<ServiceResult<TransactionResponse>> GetTransactionsAsync();
+        Task<ServiceResult> CreateTransactionAsync();
+        
+    }
+}
---
+++ backend/src/FlowTracker.Server/Services/Transaction/TransactionService.cs
@@ -0,0 +1,52 @@
+﻿using AutoMapper;
+using FlowTracker.Data.Repositories;
+using FlowTracker.Server.Services.Common;
+using FlowTracker.Shared.Dtos.Common;
+using FlowTracker.Shared.Dtos.Transaction;
+
+namespace FlowTracker.Server.Services.Transaction
+{
+    public class TransactionService : BaseService, ITransactionService
+    {
+        private readonly ITransactionRepository _transactionRepository;
+        private readonly IMapper _mapper;
+
+        public TransactionService(ITransactionRepository transactionRepository, IMapper mapper)
+        {
+            _transactionRepository = transactionRepository;
+            _mapper = mapper;
+        }
+
+
+        public Task<ServiceResult> CreateTransactionAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<ServiceResult<TransactionResponse>> GetTransaction(int id)
+        {
+            try
+            {
+                var transaction = await _transactionRepository.GetAsync(id);
+                var transactionResponse = transaction != null ? _mapper.Map<TransactionResponse>(transaction) : null;
+
+                if (transaction != null)
+                {
+                    return FailureResult<TransactionResponse>("Transaction not found", StatusCodes.Status404NotFound);
+                }
+
+                return FailureResult<TransactionResponse>("Transaction retrieved successfully", StatusCodes.Status200OK);
+            }
+            catch (Exception ex)
+            {
+                return HandleGeneralException<TransactionResponse>(ex);
+            }
+
+        }
+
+        public Task<ServiceResult<TransactionResponse>> GetTransactionsAsync()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
