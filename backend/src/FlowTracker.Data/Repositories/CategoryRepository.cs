6bd40e00f3a3ffe23b41e2ce7f567ec0ed8a8820
Carlos Blanco <acbgalan@gmail.com>
2026-04-11 03:40:48 +0200

    feat: The user is considered to prevent duplicate categories.

--- backend/src/FlowTracker.Data/Repositories/CategoryRepository.cs
+++ backend/src/FlowTracker.Data/Repositories/CategoryRepository.cs
@@ -73,9 +73,9 @@ namespace FlowTracker.Data.Repositories
             return await _context.SaveChangesAsync();
         }
 
-        public async Task<bool> ExitsByNameAndTypeAsync(string name, TransactionType type)
+        public async Task<bool> ExitsByNameAndTypeAsync(string name, TransactionType type, string userId)
         {
-            bool exits = await _context.Categories.AnyAsync(x => x.Name.ToLower() == name.ToLower() && x.Type == type);
+            bool exits = await _context.Categories.AnyAsync(x => x.Name.ToLower() == name.ToLower() && x.Type == type && x.UserId == userId);
             return exits;
         }
 
