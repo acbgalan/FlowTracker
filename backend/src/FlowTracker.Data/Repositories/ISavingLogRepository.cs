c13195c3c0b4902f61005a46307b4f4fc6b38e21
Carlos Blanco <acbgalan@gmail.com>
2026-04-14 23:35:31 +0200

    feat: SavingLog data repository

---
+++ backend/src/FlowTracker.Data/Repositories/ISavingLogRepository.cs
@@ -0,0 +1,13 @@
+﻿using FlowTracker.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FlowTracker.Data.Repositories
+{
+    public interface ISavingLogRepository : IRepositoryAsync<SavingLog>
+    {
+        Task<SavingLog?> GetAsync(int id, string userId);
+        Task<List<SavingLog>> GetAllAsync(string userId);
+    }
+}
---
+++ backend/src/FlowTracker.Data/Repositories/SavingLogRepository.cs
@@ -0,0 +1,78 @@
+﻿using FlowTracker.Data.Contexts;
+using FlowTracker.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FlowTracker.Data.Repositories
+{
+    public class SavingLogRepository : ISavingLogRepository
+    {
+        private readonly ApplicationContext _context;
+
+        public SavingLogRepository(ApplicationContext context)
+        {
+            _context = context;
+        }
+
+        public async Task AddAsync(SavingLog entity)
+        {
+            await _context.SavingLogs.AddAsync(entity);
+        }
+
+        public async Task<SavingLog?> GetAsync(int id)
+        {
+            return await _context.SavingLogs.FindAsync(id);
+        }
+
+        public async Task<SavingLog?> GetAsync(int id, string userId)
+        {
+            return await _context.SavingLogs.FirstOrDefaultAsync(x => x.Id == id && x.SavingGoal.UserId == userId);
+        }
+
+        public async Task<List<SavingLog>> GetAllAsync()
+        {
+            return await _context.SavingLogs.ToListAsync();
+        }
+
+        public async Task<List<SavingLog>> GetAllAsync(string userId)
+        {
+            return await _context.SavingLogs.Where(x => x.SavingGoal.UserId == userId).ToListAsync();
+        }
+
+        public Task UpdateAsync(SavingLog entity)
+        {
+            _context.SavingLogs.Update(entity);
+            return Task.CompletedTask;
+        }
+
+        public async Task DeleteAsync(int id)
+        {
+            var savingLog = await GetAsync(id);
+
+            if (savingLog != null)
+            {
+                _context.SavingLogs.Remove(savingLog);
+            }
+        }
+
+        public Task DeleteAsync(SavingLog entity)
+        {
+            _context.SavingLogs.Remove(entity);
+            return Task.CompletedTask;
+        }
+
+        public async Task<bool> ExitsAsync(int id)
+        {
+            return await _context.SavingLogs.AnyAsync(x => x.Id == id);
+        }
+
+        public async Task<int> SaveAsync()
+        {
+            return await _context.SaveChangesAsync();
+        }
+
+
+    }
+}
