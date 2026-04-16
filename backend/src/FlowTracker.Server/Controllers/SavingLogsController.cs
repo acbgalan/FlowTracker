6776136a59fede1bb39aba3fe13717a3b78e6bc1
Carlos Blanco <acbgalan@gmail.com>
2026-04-16 13:22:16 +0200

    feat: Added GetSavingLog and GetAllSavingLogs controller actions

--- backend/src/FlowTracker.Server/Controllers/SavingLogsController.cs
+++ backend/src/FlowTracker.Server/Controllers/SavingLogsController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using FlowTracker.Server.Services.SavingLog;
+using FlowTracker.Shared.Dtos.Common;
+using FlowTracker.Shared.Dtos.SavingLog;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace FlowTracker.Server.Controllers
@@ -7,5 +10,45 @@ namespace FlowTracker.Server.Controllers
     [ApiController]
     public class SavingLogsController : ControllerBase
     {
+        private readonly ISavingLogService _savingLogService;
+
+        public SavingLogsController(ISavingLogService savingLogService)
+        {
+            _savingLogService = savingLogService;
+        }
+
+        [HttpGet("{id:int}", Name = "GetSavingLog")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<ActionResult<SavingLogResponse>> GetSavingLog(int id)
+        {
+            var serviceResult = await _savingLogService.GetSavingLogAsync(id);
+
+            if (!serviceResult.Success)
+            {
+                return StatusCode(serviceResult.StatusCode, serviceResult.Message);
+            }
+
+            return Ok(serviceResult.Data);
+        }
+
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<ActionResult<List<SavingLogResponse>>> GetAllSavingLogs()
+        {
+            var serviceResult = await _savingLogService.GetSavingLogsAsync();
+
+            if (!serviceResult.Success)
+            {
+                return StatusCode(serviceResult.StatusCode, serviceResult.Message);
+            }
+
+            return Ok(serviceResult.Data);
+        }
+
+
+
     }
 }
