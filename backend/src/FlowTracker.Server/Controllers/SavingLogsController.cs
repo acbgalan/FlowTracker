b4f93185b48f0ce8dcaef8300825b562e00195a5
Carlos Blanco <acbgalan@gmail.com>
2026-04-16 14:05:58 +0200

    feat: A method is added to the controller to create a new SavingLog. It includes DTO validation using FluentValidation.

--- backend/src/FlowTracker.Server/Controllers/SavingLogsController.cs
+++ backend/src/FlowTracker.Server/Controllers/SavingLogsController.cs
@@ -1,6 +1,8 @@
 ﻿using FlowTracker.Server.Services.SavingLog;
 using FlowTracker.Shared.Dtos.Common;
 using FlowTracker.Shared.Dtos.SavingLog;
+using FluentValidation;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -8,13 +10,16 @@ namespace FlowTracker.Server.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
+    [Authorize]
     public class SavingLogsController : ControllerBase
     {
         private readonly ISavingLogService _savingLogService;
+        private readonly IValidator<CreateSavingLogRequest> _createSavingLogRequestValidator;
 
-        public SavingLogsController(ISavingLogService savingLogService)
+        public SavingLogsController(ISavingLogService savingLogService, IValidator<CreateSavingLogRequest> createSavingLogRequestValidator)
         {
             _savingLogService = savingLogService;
+            _createSavingLogRequestValidator = createSavingLogRequestValidator;
         }
 
         [HttpGet("{id:int}", Name = "GetSavingLog")]
@@ -46,6 +51,31 @@ namespace FlowTracker.Server.Controllers
             }
 
             return Ok(serviceResult.Data);
+        }
+
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<ActionResult<SavingLogResponse>> CreateSavingLog([FromBody] CreateSavingLogRequest createSavingLogRequest)
+        {
+            // 1. DTO validation
+            var validationResult = _createSavingLogRequestValidator.Validate(createSavingLogRequest);
+
+            if (!validationResult.IsValid)
+            {
+                return BadRequest(validationResult.ToDictionary());
+            }
+
+            // 2. Service call
+            var serviceResult = await _savingLogService.CreateSavingLogAsync(createSavingLogRequest);
+
+            if (!serviceResult.Success)
+            {
+                return StatusCode(serviceResult.StatusCode, serviceResult.Message);
+            }
+
+            return CreatedAtRoute("GetSavingLog", new { id = serviceResult.Data!.Id }, serviceResult.Data);
         }
 
 
