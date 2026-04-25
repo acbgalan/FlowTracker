f4f88bc7fe347b538cd14ef94db7aafbbd955b40
Carlos Blanco <acbgalan@gmail.com>
2026-04-25 03:40:04 +0200

    feat: layout and routing

--- frontend/flowtracker-client/src/app/app.routes.ts
+++ frontend/flowtracker-client/src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { Dashboard } from './shared/components/dashboard/dashboard';
+import { Categories } from './features/categories/categories';
+import { Transactions } from './features/transactions/transactions';
+import { SavingGoals } from './features/saving-goals/saving-goals';
+import { Reports } from './features/reports/reports';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'home', component: Dashboard },
+    { path: 'categories', component: Categories },
+    { path: 'transactions', component: Transactions },
+    { path: 'saving-goals', component: SavingGoals},
+    { path: 'reports', component: Reports },
+    { path: '', redirectTo: 'home', pathMatch: 'full' },
+    { path: '**', redirectTo: 'home' }
+];
---
+++ frontend/flowtracker-client/src/app/features/saving-goals/saving-goals.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-saving-goals',
+  imports: [],
+  templateUrl: './saving-goals.html',
+  styleUrl: './saving-goals.css',
+})
+export class SavingGoals {
+
+}
