a9199bac33f3ebaa8bfa11f0bdcad22415b00d3d
Carlos Blanco <acbgalan@gmail.com>
2026-05-02 02:41:33 +0200

    feat: login and register forms

--- frontend/flowtracker-client/src/app/features/login/login.ts
+++ frontend/flowtracker-client/src/app/features/login/login.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 @Component({
   selector: 'app-login',
   imports: [],
   templateUrl: './login.html',
   styleUrl: './login.css',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class Login {
 
